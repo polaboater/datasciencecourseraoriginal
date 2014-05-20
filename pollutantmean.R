d42ebcb2b77f97050b29c5f86e7a8ef466abdeb7
polaboater <andibarendt@gmail.com>
2014-05-20 16:14:30 -0500

    bananas

---
+++ pollutantmean.R
@@ -0,0 +1,55 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+    if (pollutant != "sulfate" & pollutant != "nitrate") { 
+        "learn to spell!"
+    } else {
+        
+        if (pollutant == "nitrate") {
+            n = 3
+        } else {
+            n = 2
+        }
+        
+        total <- vector()
+        number <- vector()
+
+                
+        for (i in id) {
+            if (i < 10) {
+                str_id <- paste("00",i,".csv",sep="")
+                str_id
+            } else if (i < 100) {
+                str_id <- paste("0",i,".csv",sep="")
+                str_id
+            } else {
+                str_id <- paste(i,".csv",sep="")
+                str_id
+            }
+            
+            dir <- paste(getwd(),"/",directory,sep="")
+            filename <- paste(dir,"/",str_id,sep="")
+        
+   
+            data <- read.csv(file = filename, header = T, sep = ",")
+            
+             w <- is.na(data[,n])
+             w <- !w
+             number <- c(number, sum(w))
+             shrink <- data[w,]
+             total <- c(total, sum(shrink[,n]))
+        }
+    
+         tot = sum(total)
+         num = sum(number)
+         pollutantmean = tot/num
+         pollutantmean
+    }
+}
+
+append <- function(directory, pollutant, id = 1:332) {
+    x <- vector()
+    for (i in id) {
+        x <- c(x,i)
+    }
+    x
+}
+    
